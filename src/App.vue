072ca8e357a2d61b5e5d8d99c631ebe351e4dc01
Denis Ostrovsky <denisostrovskis1@gmail.com>
2022-08-01 13:41:43 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <p class="absolute text-center top-0 left-1/2 -translate-x-1/2 translate-y-1/2 text-grayish-blue">Click on card to open input for generate qr code</p>
+
+  <div class="h-screen flex justify-center items-center">
+    <QRCodeCard :qrCode="textToQrCode" @click="generateInputShow = !generateInputShow" />
+  </div>
+
+  <div class="absolute inset-0 bg-gray-400 bg-opacity-70 z-50 w-full h-screen" v-show="generateInputShow">
+    <p class="text-white absolute z-50 -mt-14 -ml-32 top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2">Press Enter or Shift + Space on input to close this window</p>
+
+    <input type="text" placeholder="Enter text to generate qr code"
+      @keydown.shift.space="generateInputShow = !generateInputShow"
+      @keydown.enter="generateInputShow = !generateInputShow"
+      v-model="textToQrCode"
+      class="w-2/5 p-2 outline-none rounded-lg shadow absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 focus:border-2 focus:border-light-blue duration-150">
+  </div>
+
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Denis Ostrovsky</a>.
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+  import { defineComponent } from 'vue';
+  import QRCodeCard from '@/components/QRCodeCard.vue'
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+  export default defineComponent({
+    name: 'App',
+    components: {
+      QRCodeCard
+    },
+
+    data() {
+      return {
+        generateInputShow: false,
+        textToQrCode: ''
+      }
+    },
+  });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .attribution {
+    font-size: 11px;
+    text-align: center;
+
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    right: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .attribution a {
+    color: hsl(228, 45%, 44%);
+  }
 </style>
---
+++ src/components/QRCodeCard.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="bg-white w-[65%] lg:w-[15%] p-3 flex flex-col justify-center text-center rounded-xl space-y-4">
+        <div class="relative bg-light-blue w-full h-52 rounded-xl overflow-hidden">
+            <div class="absolute -top-24 -left-16 bg-lighter-blue bg-opacity-50 w-52 h-52 rounded-full"></div>
+            <QRCodeVue :value="qrCode" :size="qrSize" render-as="svg" class="svg-fill z-50" />
+            <div class="absolute -bottom-36 -right-24 bg-lighter-blue bg-opacity-50 w-52 h-52 rounded-full"></div>
+        </div>
+
+        <div class="space-y-2 pb-4">
+            <h1 class="font-bold text-lg text-dark-blue">Improve your front-end skills by building projects</h1>
+            <p class="text-sm text-grayish-blue">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import QRCodeVue from 'qrcode.vue'
+
+    export default {
+        components: {
+            QRCodeVue
+        },
+
+        props: {
+            qrCode: String
+        },
+
+        data() {
+            return {
+                qrSize: '120'
+            }
+        }
+    }
+</script>
+
+<style>
+    .svg-fill {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .svg-fill > path:first-child {
+        fill: transparent;
+    }
+
+    .svg-fill > path:last-child {
+        fill: #fff;
+    }
+</style>
